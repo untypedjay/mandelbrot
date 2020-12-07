25ca648f11f7d9e964e9f634f826b6399378b1b7
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-07 16:07:25 +0100

    Compute image generation on a separate thread

--- MandelbrotGenerator/AsyncImageGenerator.cs
+++ MandelbrotGenerator/AsyncImageGenerator.cs
@@ -9,18 +9,24 @@ namespace MandelbrotGenerator
     {
         public void GenerateImage(Area area)
         {
+            Thread thread = new Thread(new ParameterizedThreadStart(Run));
+            thread.Start(area);
+        }
+
+        public event EventHandler<EventArgs<Tuple<Area, Bitmap, TimeSpan>>> ImageGenerated;
+        private void OnImageGenerated(Area area, Bitmap bitmap, TimeSpan timespan)
+        {
+            ImageGenerated?.Invoke(this, new EventArgs<Tuple<Area, Bitmap, TimeSpan>>(new Tuple<Area, Bitmap, TimeSpan>(area, bitmap, timespan)));
+        }
+
+        private void Run(object a)
+        {
+            Area area = (Area)a;
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             var bitmap = SyncImageGenerator.GenerateImage(area);
             stopwatch.Stop();
             OnImageGenerated(area, bitmap, stopwatch.Elapsed);
-        }
-
-        public event EventHandler<EventArgs<Tuple<Area, Bitmap, TimeSpan>>> ImageGenerated;
-
-        private void OnImageGenerated(Area area, Bitmap bitmap, TimeSpan timespan)
-        {
-            ImageGenerated?.Invoke(this, new EventArgs<Tuple<Area, Bitmap, TimeSpan>>(new Tuple<Area, Bitmap, TimeSpan>(area, bitmap, timespan)));
         }
     }
 }
